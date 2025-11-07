09e12f0c71f2175a69084893c7febe385da9f211
KShaikh <shaikhtj1477@gmail.com>
2025-11-07 09:55:59 +0530

    multilevel inheritance

--- MultilevelInheritance.java
+++ MultilevelInheritance.java
@@ -1,24 +1,24 @@
 class School{
-    void School(){
+    void school(){
         System.out.println("this is school");
     }
 }
 class Teacher extends School{
-    void Teacher(){
+    void teacher(){
         System.out.println(" the Teacher teaches in  the school");
     }
 }
 class Student extends Teacher{
-    void Student(){
+    void student(){
         System.out.println(" the Student studies in  the school");
     }
 }
 public class MultilevelInheritance {
     public static void main(String[] args){
         Student student = new Student();
-        student.School();
-        student.Teacher();
-        student.Student();
+        student.school();
+        student.teacher();
+        student.student();
     }
     
 }
