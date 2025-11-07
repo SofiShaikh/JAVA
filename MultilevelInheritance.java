8979a9500a93ee4a6a711817dd0ba047ec8c502b
SofiShaikh <sofishaikh8786@gmail.com>
2025-11-07 11:55:33 +0530

    MultilevelInheritance

--- MultilevelInheritance.java
+++ MultilevelInheritance.java
@@ -1,24 +1,24 @@
-interface Teacher{
+interface Teacher {
     void teacher();
 }
-interface Student{
+
+interface Student {
     void student();
 }
-class School implements Teacher,Student{
-    public void teacher(){
+
+class School implements Teacher, Student {
+    public void teacher() {
         System.out.println("The teacher teaches in the school");
     }
-    public void student(){
+    public void student() {
         System.out.println("The student studies in the school");
     }
-
 }
-public class MultilevelInheritance{
+
+public class MultilevelInheritance {
     public static void main(String[] args) {
-        School school=new School();
+        School school = new School();
         school.teacher();
         school.student();
     }
 }
-    
-
